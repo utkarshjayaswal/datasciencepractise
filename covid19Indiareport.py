f7c4ec0952df6cf6eca8516a4de3ebe4617132fd
utkarshjayaswal <utkarsh.jayaswal@outlook.com>
2022-01-19 19:05:57 +0530

    reated covid report for india using a different csv file

---
+++ covid19Indiareport.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv('covid_19_india.csv')
+plt.figure(figsize=(10, 5))
+plt.xlabel('State/UnionTerritory')
+plt.ylabel('Cured')
+plt.plot(data.StateUnionTerritory, data.Cured, 'y.-', label='cumulative')
+plt.xticks(data.StateUnionTerritory[::150])
+plt.legend()
+plt.show()
